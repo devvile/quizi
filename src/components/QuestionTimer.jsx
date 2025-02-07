ec4273135c9f4160fb24f9f02b898855426d330f
devvile <patryk.czemierowski@gmail.com>
2025-02-07 15:30:03 +0100

    timer added

--- src/components/QuestionTimer.jsx
+++ src/components/QuestionTimer.jsx
@@ -1,9 +1,23 @@
-export default function QuestionTimer({timeout}) {
-    
-  setTimeout(() => {}, timeout);
-  return (
-    <div id="timer">
-      <h2>Time Remaining: 10</h2>
-    </div>
+import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+
+export default function QuestionTimer({timeout, onTimeout}) {    
+  const [remaningTime,setRemainingTime ] =useState(timeout);
+
+  useEffect(() => { console.log('setting timeout'); const timeout2 = setTimeout(onTimeout, timeout);
+    return () => clearInterval(timeout2);
+  },[ onTimeout]);
+  useEffect(() => { console.log('setting interval'); const interval = setInterval( ()=>{setRemainingTime((prevRemainingTime)=>prevRemainingTime-100);}, 100);
+    return ()=>{clearInterval(interval);};
+  },[]);
+
+  return (<progress id="question-time" max={timeout} value={remaningTime}/>
   );
 }
+
+
+// Deklaracja PropTypes pod komponentem
+QuestionTimer.propTypes = {
+  timeout: PropTypes.number.isRequired,
+  onTimeout: PropTypes.func.isRequired
+};
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,32 +1,38 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../../data/questions';
+import QuestionTimer from '../QuestionTimer';
 
 export default function Quiz() {
-    const [userAnswers, setUserAnswers] = useState([]);
-    const activeQuestionIndex= userAnswers.length;
-    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
-    const results = (
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex= userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+  const results = (
     <div id="summary"> 
-        <h2>Quiz Completed</h2>
-    </div>)
+      <h2>Quiz Completed</h2>
+    </div>);
 
-    if (quizIsComplete) {
-        return results;
-    }
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(answer)  {
+         setUserAnswers((prevUserAnswers)=>{ return [...userAnswers, answer]})
+        },[]);
+    const handleSkipAnswer = useCallback(()=>{handleSelectAnswer(null),[handleSelectAnswer]});
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(()=>Math.random()-0.5);
-    const handleSelectAnswer = (answer) => { setUserAnswers([...userAnswers, answer]);};
+  if (quizIsComplete) {
+    return results;
+  }
 
-    const quizBody = (  <div id="quiz">
-        <div id="question">
-            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-            <ul id="answers">
-                {shuffledAnswers.map((answer, index) => 
-                <li key={index} className="answer">
-                <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button></li>)}
-            </ul> 
-        </div>
-      </div>)
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(()=>Math.random()-0.5);
 
-    return quizBody;
+  const quizBody = (  <div id="quiz">
+    <div id="question">
+        <QuestionTimer key= {activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer}/>
+      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+      <ul id="answers">
+        {shuffledAnswers.map((answer, index) => 
+          <li key={index} className="answer">
+            <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button></li>)}
+      </ul> 
+    </div>
+  </div>);
+
+  return quizBody;
 }
